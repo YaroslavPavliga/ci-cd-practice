e456e1f13541b3af073d79b64dd760b76c078723
YaroslavPavliga <st9260851@stud.duikt.edu.ua>
2025-12-26 13:07:53 +0200

    Update sample.test.js

--- test/sample.test.js
+++ test/sample.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert');
 
-describe('Sample test', () => {
-  it('should pass', () => {
-    assert.strictEqual(1 + 1, 2);
+describe('Sample Test', function() {
+  it('should pass', function() {
+    assert.strictEqual(1, 1);
   });
 });
